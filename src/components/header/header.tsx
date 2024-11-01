e6ec6f8114968aa48724e1cdc1f64c8a9ced573b
Bamlakw <153365618+Bamlakw@users.noreply.github.com>
2024-11-01 14:33:56 +0300

    Describe the changes you made

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,8 @@ import classNames from 'classnames';
 import { Link, NavLink } from '@remix-run/react';
 import { ROUTES } from '~/router/config';
 import styles from './header.module.scss';
+import Group1Png from '../../assets/group 1.png';
+import ButtonImagePng from '../../assets/button image.png';
 
 export interface HeaderProps {
     className?: string;
@@ -11,7 +13,7 @@ export const Header = ({ className }: HeaderProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <Link to="/" className={styles.logo}>
-                LOGO
+                <img src={Group1Png} alt="" className={styles.img1} />
             </Link>
             <div className={styles.menu}>
                 <NavLink
@@ -20,7 +22,7 @@ export const Header = ({ className }: HeaderProps) => {
                         classNames(styles.menuButton, { [styles.active]: isActive })
                     }
                 >
-                    Home
+                    Open for work
                 </NavLink>
                 <NavLink
                     to={ROUTES.about.to()}
@@ -28,7 +30,14 @@ export const Header = ({ className }: HeaderProps) => {
                         classNames(styles.menuButton, { [styles.active]: isActive })
                     }
                 >
-                    About
+                    <button className={classNames(styles.button1, styles.button1)}>
+                        <img
+                            className={classNames(styles.img2, styles.img2)}
+                            src={ButtonImagePng}
+                            alt='buttons'
+                        />
+                        Start a Project
+                    </button>
                 </NavLink>
             </div>
         </div>
---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,18 @@
+import styles from './hero-section.module.scss';
+import cx from 'classnames';
+
+export interface HeroSectionProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const HeroSection = ({ className }: HeroSectionProps) => {
+    return (
+        <div>
+            <div />
+        </div>
+    );
+};
